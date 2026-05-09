ef71e31e35c0e7c662ef8c85ceab1eec5457929d
Giorgi Chokoshvili <giochoko2006@gmail.com>
2026-05-09 14:33:28 +0400

    init/backend: project backend initialized with spring boot and docker

---
+++ Backend/src/test/java/org/gitbounty/gitbountybackend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gitbounty.gitbountybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
