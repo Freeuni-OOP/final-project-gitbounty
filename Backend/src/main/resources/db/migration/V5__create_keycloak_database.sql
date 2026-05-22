f76f219ffa2869d5d1163c9401b319ecca4e152d
Giorgi Chokoshvili <giochoko2006@gmail.com>
2026-05-23 01:19:45 +0400

    deleted unwanted migration file

--- Backend/src/main/resources/db/migration/V5__create_keycloak_database.sql
+++
@@ -1,9 +0,0 @@
--- V5__create_keycloak_database.sql
--- Creates the Keycloak database and grants a dedicated user access to it.
-
-CREATE DATABASE IF NOT EXISTS keycloak_dev CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-CREATE USER IF NOT EXISTS 'keycloak_user'@'%' IDENTIFIED BY 'keycloak_password';
-GRANT ALL PRIVILEGES ON keycloak_dev.* TO 'keycloak_user'@'%';
-FLUSH PRIVILEGES;
-
