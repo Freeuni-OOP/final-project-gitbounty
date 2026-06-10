ac639a604ff68150b5c6be6c3ea4002f0d739456
Giorgi Chokoshvili <giochoko2006@gmail.com>
2026-06-11 02:52:50 +0400

    quick fix

--- Backend/src/main/java/org/gitbounty/gitbountybackend/controller/Codebase/pullRequest/PullRequestController.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/controller/Codebase/pullRequest/PullRequestController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequestMapping("/api/codebases/{repositoryName}/pull-requests")
 class PullRequestController {
     private final PullRequestService pullRequestService;
