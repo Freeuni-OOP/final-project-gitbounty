46e5017840b72fb107ae3ca2c71f9a3deb97fc8c
Giorgi Chokoshvili <giochoko2006@gmail.com>
2026-06-11 02:53:07 +0400

    quick fix 2

--- Backend/src/main/java/org/gitbounty/gitbountybackend/controller/Codebase/pullRequest/PullRequestController.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/controller/Codebase/pullRequest/PullRequestController.java
@@ -6,7 +6,6 @@ import org.gitbounty.gitbountybackend.service.codebase.issue.pullRequest.PullReq
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
