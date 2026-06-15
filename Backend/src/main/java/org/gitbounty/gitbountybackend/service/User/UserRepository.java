3f5aeebfd2dab7b5e00080e0b791bb19fbb67af8
nika lomsadze <nikalomsadze056@gmail.com>
2026-06-15 17:38:26 +0400

    Change interface to public

--- Backend/src/main/java/org/gitbounty/gitbountybackend/service/User/UserRepository.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/service/User/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
      * Find a user by username.
