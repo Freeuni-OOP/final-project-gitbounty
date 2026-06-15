67b4f4e088e163413e4d3a95734efba95487a9ae
katdevv <katosheshelidze@gmail.com>
2026-06-15 23:06:20 +0400

    feat: implement global exception handling and clean up UserController closes #66

--- Backend/src/main/java/org/gitbounty/gitbountybackend/exception/DuplicateUserException.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/exception/DuplicateUserException.java
@@ -1,9 +1,8 @@
 package org.gitbounty.gitbountybackend.exception;
 
-public class DuplicateUserException extends Exception {
+public class DuplicateUserException extends RuntimeException {
 
     public DuplicateUserException(String message) {
         super(message);
     }
 }
-
