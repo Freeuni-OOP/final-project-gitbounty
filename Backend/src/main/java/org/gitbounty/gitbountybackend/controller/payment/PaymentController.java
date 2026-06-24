f1dbfd72f917c918d07f1432b93298e3a37b0720
nika lomsadze <nikalomsadze056@gmail.com>
2026-06-25 00:50:03 +0400

    Remove @Valid as it wasn't working

--- Backend/src/main/java/org/gitbounty/gitbountybackend/controller/payment/PaymentController.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/controller/payment/PaymentController.java
@@ -8,7 +8,6 @@ import org.gitbounty.gitbountybackend.model.User;
 import org.gitbounty.gitbountybackend.service.payment.CreateCreditTopUpCommand;
 import org.gitbounty.gitbountybackend.service.payment.CreditTopUpPaymentService;
 import org.gitbounty.gitbountybackend.service.user.UserService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -34,7 +33,7 @@ public class PaymentController {
 
     @PostMapping("/credit-topups")
     public ResponseEntity<CreditTopUpPaymentResponse> createCreditTopUp(
-        @Valid @RequestBody CreateCreditTopUpRequest request,
+        @RequestBody CreateCreditTopUpRequest request,
         @AuthenticationPrincipal Jwt jwt
     ) {
         CreditTopUpPaymentResponse response = CreditTopUpPaymentResponse.from(
