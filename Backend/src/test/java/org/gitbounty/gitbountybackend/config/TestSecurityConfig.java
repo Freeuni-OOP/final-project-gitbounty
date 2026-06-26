ce1fde41ff8bf7e7541e3d891c210ccf083d6e54
Giorgi Chokoshvili <giochoko2006@gmail.com>
2026-06-26 15:21:34 +0400

    fix: Tests need accounting for CORS. created TestSecurityConfig class

---
+++ Backend/src/test/java/org/gitbounty/gitbountybackend/config/TestSecurityConfig.java
@@ -0,0 +1,21 @@
+package org.gitbounty.gitbountybackend.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+@EnableWebSecurity
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable) // Disable CORS in test
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Permit all
+            .build();
+    }
+}
