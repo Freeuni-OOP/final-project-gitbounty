54a1881a779fcae4fec8116b950778fc9f58c5f7
gigichachua <gigichachua48@gmail.com>
2026-06-29 15:18:34 +0400

    improved test

--- Backend/src/test/java/org/gitbounty/gitbountybackend/controller/bounty/BountyControllerIntegrationTest.java
+++ Backend/src/test/java/org/gitbounty/gitbountybackend/controller/bounty/BountyControllerIntegrationTest.java
@@ -1,9 +1,11 @@
 package org.gitbounty.gitbountybackend.controller.bounty;
 
+import org.gitbounty.gitbountybackend.config.SecurityConfig;
 import org.gitbounty.gitbountybackend.service.bounty.BountyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BountyController.class)
+@Import(SecurityConfig.class)
 public class BountyControllerIntegrationTest {
 
     @Autowired
